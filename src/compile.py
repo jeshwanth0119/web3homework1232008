ae701a05e8e7664386589f962a3c00bb73dc3ad5
jeshwanth0119 <jamgoth@my.bridgeport.edu>
2024-11-07 12:38:39 -0500

    final version

---
+++ src/compile.py
@@ -0,0 +1,21 @@
+from solcx import compile_standard 
+import json
+
+SOLIDITY_PRAGMA = "0.8.13"
+
+def Compile_Solidity(contract: str)-> str:
+     
+     with open(contract,'r') as file: 
+         contract_file = file.read() 
+
+     compiled_sol = compile_standard(
+          { "language": "Solidity", 
+           "sources": {contract: {"content": contract_file}},
+             "settings": { "outputSelection": 
+                          {"*": {"*":["abi","evm.bytecode"]}} }, }, 
+                          solc_version=SOLIDITY_PRAGMA) 
+     return compiled_sol
+if __name__ == "__main__":
+     Compiled_sol = Compile_Solidity("./src/newContract.sol")
+     with open('./src/Compiled/newContract.json','w') as file:
+          json.dump(Compiled_sol,file)
